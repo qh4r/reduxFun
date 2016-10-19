1000ed22154ccf2b4436eac09880b7def548e97a
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-10-19 20:48:31 +0200

    Finished up search bar and set up api

--- weatherApp/src/containers/search_bar.js
+++ weatherApp/src/containers/search_bar.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+    }
+
     render() {
-        return (<form className="input-group">
-            <input className="form-control" type="text"/>
+        return (<form onSubmit={this.onFormSubmit.bind(this)} className="input-group">
+            <input onChange={e => this.onInputChange(e)}
+                   placeholder="Wpisz miasto dla którego chcesz otrzymać prognozę"
+                   value={this.state.term}
+                   className="form-control" type="text"/>
             <span className="input-group-btn">
                 <button type="submit" className="btn btn-secondary">Wyślij</button>
                 </span>
         </form>)
+    }
+
+    onInputChange(event) {
+        this.setState({
+            term: event.target.value
+        });
+        console.log(event.target.value);
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
     }
 }
