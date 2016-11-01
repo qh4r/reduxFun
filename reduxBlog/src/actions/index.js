af875fbe50aba8aac1574dd6e2dde54ee654b02b
Rafa³ Kucharski <r.kucharski@lgbs.pl>
2016-11-01 16:14:01 +0100

    Changed storage to firebase (using valid redux instead of reactfire)

--- reduxBlog/src/actions/index.js
+++ reduxBlog/src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import firebase from 'firebase';
+import uuid from 'uuid';
 
 export const FETCH_POSTS = Symbol('FETCH_POSTS');
 export const CREATE_POST = Symbol('CREATE_POST');
@@ -7,11 +9,17 @@ export const DELETE_POST = Symbol('DELETE_POST');
 
 import rootUrl from './root_url'
 
+// 2 krokowa inicjalizacja firebase
+firebase.initializeApp({
+    databaseURL: 'https://blogtest-7e122.firebaseio.com/'
+});
+
+const Posts = firebase.database().ref();
 // PRZYKłAD UZYCIA REDUX-THUNK
 // pozwala dispatchowac - bardziej niskopoziomowy middleware niz redux promise
 // musi byc uzyty w combine middlewares by dzialac
 // wykrywa gdy akcja jest dunkcja i traktuja ja jako akcja z callbackiem (callback to dispatch)
-export function fetchPosts() {
+function fetchPostsOld() {
     const request = axios.get(rootUrl('/posts'));
 
     return (dispatch) => {
@@ -24,7 +32,19 @@ export function fetchPosts() {
     }
 }
 
-export function createPost(props) {
+//using firebase
+export function fetchPosts() {
+    return dispatch => {
+        Posts.on('value', snapshot => {
+            dispatch({
+                type: FETCH_POSTS,
+                payload: snapshot.val()
+            });
+        })
+    }
+}
+
+function createPostOld(props) {
     const request = axios.post(rootUrl('/posts'), props);
     console.log('submit', props);
     return {
@@ -33,7 +53,21 @@ export function createPost(props) {
     }
 }
 
-export function findPost(id) {
+// mozna zwrocic promise dzieki middlewarowi promise
+export function createPost(props){
+    return new Promise(resolve => {
+        resolve({
+            type: CREATE_POST,
+            payload: Posts.push(props)
+        })
+    });
+    // return {
+    //     type: CREATE_POST,
+    //     payload: new Promise((x) => x(Posts.push(props)))
+    // }
+}
+
+function findPostOld(id) {
     const request = axios.get(rootUrl(`/posts/${id}`));
     return {
         type: FIND_POST,
@@ -41,10 +75,27 @@ export function findPost(id) {
     }
 }
 
-export function deletePost(id) {
+export function findPost(id) {
+    return dispatch => {
+        Posts.child(id).on('value', data => {
+            dispatch({
+                type: FIND_POST,
+                payload: data.val()
+            })
+        })
+    }
+}
+function deletePostOld(id) {
     const request = axios.delete(rootUrl(`/posts/${id}`));
     return {
         type: DELETE_POST,
         payload: request
+    }
+}
+
+export function deletePost(id) {
+    return {
+        type: DELETE_POST,
+        payload: Posts.child(id).remove()
     }
 }
--- reduxBlog/src/reducers/reducer_posts.js
+++ reduxBlog/src/reducers/reducer_posts.js
@@ -6,19 +6,26 @@ export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
         case FETCH_POSTS:
             // var test =  Object.assign(state, {all: action.payload.data}); /// TO EJST PLYTKA KOPIA, MUTACJA, BLAD!!!
-            var test =  Object.assign({}, state, {all: action.payload}); /// tutaj podstawa to {}
+            var test = Object.assign({}, state, {all: mapFromFirebase(action.payload)}); /// tutaj podstawa to {}
             // redux wykrywa kopie?
-            console.log('vv',test);
+            console.log('vv', test);
             // var test =  {...state, all: action.payload.data};
             // console.log('vv',test);
             return test;
         case FIND_POST:
             // let post = _.find(state.all, {id: action.payload});
-            let post = action.payload.data;
+            let post = action.payload;
             return Object.assign({}, state, {post});
-            // rownowaznik
-            // return {...state, post: post};
+        // rownowaznik
+        // return {...state, post: post};
         default:
             return state;
     }
+}
+
+function mapFromFirebase(data) {
+    return data ?
+        Object.keys(data).map((key) => {
+            return {...data[key], id: key}
+        }) : [];
 }
