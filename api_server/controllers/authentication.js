465efbf2c03f0a9d5e7375e4208e5e48be2d61ca
Rafał Kucharski <r.kucharski@lgbs.pl>
2017-03-08 00:35:32 +0100

    Added encryption and salting of password

--- api_server/controllers/authentication.js
+++ api_server/controllers/authentication.js
@@ -1,15 +1,31 @@
 const UserModel = require("../models/user");
 
 module.exports = {
-    signup: function(req, res, next) {
-        const {email, password } = req.body;
+    signup: function (req, res, next) {
+        const {email, password} = req.body;
+        if(!email || !password) {
+            return res.status(422).send({error: "wymagane email i haslo"})
+        }
         UserModel.findOne({email}, (err, user) => {
+            if (err) {
+                return next(err);
+            }
 
-        });
+            if(user) {
+                return res.status(422).send({error: `Mail ${email} już w użyciu`});
+            }
 
-        res.send({
-            success: true,
-            body: req.body
+            const newUser = new UserModel({
+                email,
+                password
+            });
+
+            return newUser.save((err, result) => {
+                if(err) {
+                    return next(err);
+                }
+                return res.json({success: true});
+            });
         });
     }
 }
--- api_server/models/user.js
+++ api_server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt-nodejs");
 
 const userSchema = new mongoose.Schema({
     // lowercase sprawia ze string jest automatycznie lowercasowany
@@ -6,6 +7,25 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-const UserClass =  mongoose.model('user', userSchema);
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) {
+                return next(err);
+            }
+
+            user.password = hash;
+            next();
+        })
+    })
+});
+
+const UserClass = mongoose.model('user', userSchema);
 
 module.exports = UserClass;
