dbed0ac28ebc7716879f1ed4c15305876c7edfff
Rafał Kucharski <r.kucharski@lgbs.pl>
2017-06-23 23:45:26 +0200

    Added custom combine reducers implementation

--- redux_messenger/src/getStore.js
+++ redux_messenger/src/getStore.js
@@ -1,9 +1,26 @@
 import {
-    createStore
+  applyMiddleware,
+  createStore,
 } from 'redux';
+import { users } from '../server/db/User';
+import { getDefaultState } from '../server/getDefaultState';
+import { initializeDB } from '../server/db/initializeDB';
+import { fromJS } from 'immutable';
+import { createLogger } from 'redux-logger';
+
+initializeDB();
+
+const currentUser = users[0];
+
 
 const reducer = state => state;
 
-const store = createStore(reducer);
+const defaultState = fromJS(getDefaultState(currentUser));
 
-export const getStore = ()=>store;
+console.log(defaultState)
+
+const store = createStore(reducer, defaultState, applyMiddleware(createLogger({
+  stateTransformer: state => state.toJS(),
+})));
+
+export const getStore = () => store;
